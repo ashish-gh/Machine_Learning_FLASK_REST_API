be9e8b61decee4ff706ec04a0a3aef1e14765edd
ashish ghimire <ashish.gh123@gmail.com>
2020-02-03 13:28:32 +0545

    Adding models in app directory

--- webaap/app.py
+++ webaap/app.py
@@ -5,6 +5,14 @@ import flask
 # App defination
 app = Flask(__name__,template_folder='templates')
 
+# importing models
+with open('webapp/model/model.pkl', 'rb') as f:
+    classifier = pickle.load (f)
+
+with open('webapp/model/model_columns.pkl', 'rb') as f:
+    model_columns = pickle.load (f)
+
+
 @app.route('/')
 def welcome():
     return "Boston Housing Price Prediction"
